8f1938db2319e53b308a318eb905c24a84493a4a
Mariusz Rak <rak.m.rak@gmail.com>
2017-02-13 20:46:23 +0100

    initial commit

---
+++ components/Contacts.js
@@ -0,0 +1,17 @@
+Contacts = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
+  render: function () {
+    var contacts = this.props.items.map(function (contact) {
+      return (
+        <Contact item={contact} key={contact.id} />
+      );
+    });
+
+    return (
+      <ul className='contactsList'> {contacts}</ul>
+    );
+  }
+});
